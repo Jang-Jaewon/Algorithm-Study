2ff2a3583dca084fbd108eae2be49dfff4a68490
Jang-Jaewon <jewon119@gmail.com>
2022-03-15 16:32:34 +0900

    Add: 최소힙 & 최대힙, 디렉토리 이름 수정

---
+++ 08_heap/001_최소힙.py
@@ -0,0 +1,20 @@
+# 최소힙
+import heapq
+
+def solution():
+  res = []
+  l = []
+  while True:
+    num = int(input())
+    if num == -1:
+      return res
+    if num  == 0:
+      if len(l) == 0:
+        res.append(-1)
+      else:
+        res.append(heapq.heappop(l))
+    else:
+      heapq.heappush(l, num)
+    # print(f"l:{l}, res:{res}")
+      
+print(solution())
