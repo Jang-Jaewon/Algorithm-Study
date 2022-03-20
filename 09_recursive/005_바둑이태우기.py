10b9d65e6884c24f424f16245a2829873983b954
Jang-Jaewon <jewon119@gmail.com>
2022-03-20 15:11:54 +0900

    Add: 재귀함수 최적화

--- 09_recursive/005_바둑이태우기.py
+++ 09_recursive/005_바둑이태우기.py
@@ -3,20 +3,23 @@
 # 풀이1
 C, N = 259, 5
 l = [81, 58, 42, 33, 61]
+total = sum(l)
 res = 0
-def solution(i, sum_val):
-  global res
+def solution(i, sum_val, total_sum):
+  global res, total
+  if sum_val + (total - total_sum) < res:
+    return
   if sum_val > C:
     return
   if i == N:
     if sum_val >= res:
       res = sum_val
   else:
-      solution(i+1, sum_val + l[i])
-      solution(i+1, sum_val)
+      solution(i+1, sum_val + l[i], total_sum + l[i])
+      solution(i+1, sum_val, total_sum + l[i])
   return res
     
-print(solution(0, 0))
+print(solution(0, 0, 0))
 
 # 풀이2 
 from itertools import combinations
