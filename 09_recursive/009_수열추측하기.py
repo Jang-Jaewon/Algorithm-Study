778a2c30b9250199025dbc46374310e9a6a726db
Jang-Jaewon <jewon119@gmail.com>
2022-03-29 11:43:15 +0900

    Add: 수열추측하기 풀이 추가

--- 09_recursive/009_수열추측하기.py
+++ 09_recursive/009_수열추측하기.py
@@ -1,5 +1,6 @@
 # 수열 추측하기(파스칼 응용)
 
+#풀이1
 # N, final = map(int, input().split())
 N, final = 4, 16
 p = [0] * N # 순열
@@ -26,3 +27,22 @@ def soulution(value, sum_val):
         check_list[i] = 0
 
 soulution(0, 0)
+
+# 풀이2
+from itertools import permutations
+
+def solution(N, final):
+  b = [1] * N # 이항계수
+  for i in range(1, N):
+    b[i] = b[i-1] * (N-i) // i
+  array = [i for i in range(1, N+1)]  
+  for i in permutations(array):
+    sum = 0
+    for index, value in enumerate(i):
+      sum += (b[index] * value)
+    if sum == final:
+      return list(i)
+
+# N, final = map(int, input().split())
+N, final = 4, 16      
+print(solution(N, final))
